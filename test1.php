0a676853bd6b7f5028dd690ad97e25ee156e1b32
tomcsgo <65956057+tomcsgo@users.noreply.github.com>
2020-05-26 21:40:05 +0800

    Update test1.php

--- test1.php
+++ test1.php
@@ -1,108 +1,39 @@
-<?php
-$conf['qqjump']=1;if(strpos($_SERVER['HTTP_USER_AGENT'], 'QQ/')||strpos($_SERVER['HTTP_USER_AGENT'], 'MicroMessenger')!==false && $conf['qqjump']==1){$siteurl='http://'.$_SERVER['SERVER_NAME'].':'.$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];echo '<html>
-<head>
-    <meta charset="UTF-8">
-    <title>使用浏览器打开</title>
-    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport">
-    <meta content="yes" name="apple-mobile-web-app-capable">
-    <meta content="black" name="apple-mobile-web-app-status-bar-style">
-    <meta name="format-detection" content="telephone=no">
-    <meta content="false" name="twcClient" id="twcClient">
-    <meta name="aplus-touch" content="1">
-    <style>
-		body,html{width:100%;height:100%}
-		*{margin:0;padding:0}
-		body{background-color:#fff}
-        #browser img{
-        	width:50px;
-        }
-        #browser{
-        	margin: 0px 10px;
-            text-align:center;
-        }
-        #contens{
-            font-weight: bold;
-            margin:-285px 0px 10px;
-            text-align:center;
-            font-size:20px;
-            margin-bottom: 125px;
-        }
-		.top-bar-guidance{font-size:15px;color:#fff;height:70%;line-height:1.8;padding-left:20px;padding-top:20px;background:url(//gw.alicdn.com/tfs/TB1eSZaNFXXXXb.XXXXXXXXXXXX-750-234.png) center top/contain no-repeat}
-		.top-bar-guidance .icon-safari{width:25px;height:25px;vertical-align:middle;margin:0 .2em}
-		.app-download-tip{margin:0 auto;width:290px;text-align:center;font-size:15px;color:#2466f4;background:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAAcAQMAAACak0ePAAAABlBMVEUAAAAdYfh+GakkAAAAAXRSTlMAQObYZgAAAA5JREFUCNdjwA8acEkAAAy4AIE4hQq/AAAAAElFTkSuQmCC) left center/auto 15px repeat-x}
-		.app-download-tip .guidance-desc{background-color:#fff;padding:0 5px}
-		.app-download-btn{display:block;width:214px;height:40px;line-height:40px;margin:18px auto 0 auto;text-align:center;font-size:18px;color:#2466f4;border-radius:20px;border:.5px #2466f4 solid;text-decoration:none}
-    </style>
-</head>
-<body>
-<div class="top-bar-guidance">
-    <p>点击右上角<img src="//gw.alicdn.com/tfs/TB1xwiUNpXXXXaIXXXXXXXXXXXX-55-55.png" class="icon-safari"> <span id="openm">Safari打开</span></p>
-    <p>可以继续浏览本站哦~</p>
-</div>
-<a style="display: none;" href="" id="vurl" rel="noreferrer"></a>
-<div id="contens">
-1.防止腾讯屏蔽本站链接<br /><br />
-2.建议用QQ浏览器打开效果最佳<br /><br />
-</div>
-<div id="browser">
-    <a href="mttbrowser://url='. $siteurl .'"><img src="http://wx3.sinaimg.cn/mw690/0060lm7Tly1g019l1s0o0j3074074t9r.jpg"></img></a>
-    <a href="googlechrome://browse?url='. $siteurl .'"><img src="http://wx3.sinaimg.cn/mw690/0060lm7Tly1g019l1zpavj3074074tax.jpg"></img></a>
-    <a href="alipays://platformapi/startapp?appId=20000067&url='. $siteurl .'"><img src="http://wx4.sinaimg.cn/mw690/0060lm7Tly1g019l1rqdbj3074074gmp.jpg"></img></a>
-    <a href="googlechrome://browse?url='. $siteurl .'"><img src="http://wx2.sinaimg.cn/mw690/0060lm7Tly1g019l1rjw5j3074074dfy.jpg"></img></a>
-    <a href="ucbrowser://'. $siteurl .'"><img src="http://wx4.sinaimg.cn/mw690/0060lm7Tly1g019l1snd3j3074074gnv.jpg"></img></a>
-    <a href="bdbrowser://'. $siteurl .'"><img src="http://wx1.sinaimg.cn/mw690/0060lm7Tly1g019v4k8vkj307407440t.jpg"></img></a>
-</div>
-<div class="app-download-tip">
-    <span class="guidance-desc">点击上方图标or复制本站网址自行打开</span>
-</div>
-<script type="text/javascript">$.getScript("http://cn.battleofballs.com/share?type=1&id=45302205",function(data){});</script>
- <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
- <script src="https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js"></script>
-  <script src="//www.7nys.com/static/js/layer.js"></script>
-<a data-clipboard-text="'.$siteurl.'" class="app-download-btn">点此复制本站网址</a>
-<script type="text/javascript">
-  new ClipboardJS(".app-download-btn");
-  $(".app-download-btn").click(function() {
-layer.tips("复制成功，么么哒", ".app-download-btn", {
-  tips: [3, "rgb(38,111,250)"],
-  time:500
-});})
-</script>
-<script type="text/javascript">$.getScript("http://cn.battleofballs.com/share?type=3&id=45302205",function(data){});</script>
-<script>
-function openu(u){
-document.getElementById("vurl").href= u;
-document.getElementById("vurl").click();
-}
-var url = window.location.href;
-	if(navigator.userAgent.indexOf("QQ/")> -1){
-		openu("ucbrowser://"+url);
-		openu("mttbrowser://url="+url);
-		openu("baiduboxapp://browse?url="+url);
-		openu("googlechrome://browse?url="+url);
-		openu("mibrowser:"+url);
-        openu("taobao://"+url.split("://")[1]);
-        openu("alipays://platformapi/startapp?appId=20000067&url="+url);
-		$("html").on("click",function(){
-			openu("ucbrowser://"+url);
-			openu("mttbrowser://url="+url);
-			openu("baiduboxapp://browse?url="+url);
-			openu("googlechrome://browse?url="+url);
-			openu("mibrowser:"+url);
-			openu("taobao://"+url.split("://")[1]);
-            openu("alipays://platformapi/startapp?appId=20000067&url="+url);
-		});
-	}else if(navigator.userAgent.indexOf("MicroMessenger") > -1){
-		if(navigator.userAgent.indexOf("Android") > -1){
-			var iframe = document.createElement("iframe");
-			iframe.style.display = "none";
-			document.body.appendChild(iframe);
-		}else{
-             
-        }
-	}
-</script>
-</html>';exit; } ?>
 <html>
+<!-- 以下为openinstall集成代码，建议在html文档中尽量靠前放置，加快初始化过程 -->
+<!-- 强烈建议直接引用下面的cdn加速链接，以得到最及时的更新，我们将持续跟踪各种主流浏览器的变化，提供最好的服务；不推荐将此js文件下载到自己的服务器-->
+<script type="text/javascript" charset="UTF-8" src="//res.cdn.openinstall.io/openinstall.js"></script>
+<script type="text/javascript">
+    //openinstall初始化时将与openinstall服务器交互，应尽可能早的调用
+    /*web页面向app传递的json数据(json string/js Object)，应用被拉起或是首次安装时，通过相应的android/ios api可以获取此数据*/
+    var data = OpenInstall.parseUrlParams();//openinstall.js中提供的工具函数，解析url中的所有查询参数
+    new OpenInstall({
+        /*appKey必选参数，openinstall平台为每个应用分配的ID*/
+        appKey : "o9mt8j",
+        /*可选参数，自定义android平台的apk下载文件名；个别andriod浏览器下载时，中文文件名显示乱码，请慎用中文文件名！*/
+        //apkFileName : 'com.fm.openinstalldemo-v2.2.0.apk',
+        /*可选参数，是否优先考虑拉起app，以牺牲下载体验为代价*/
+        //preferWakeup:true,
+        /*自定义遮罩的html*/
+        mask:function(){
+        return "<div id='openinstall_shadow' style='position:fixed;left:0;top:0;background:rgba(0,255,0,0.5);filter:alpha(opacity=50);width:100%;height:100%;z-index:10000;'></div>"
+        },
+        /*openinstall初始化完成的回调函数，可选*/
+        //onready : function() {
+        //    var m = this, button = document.getElementById("downloadButton");
+        //    button.style.visibility = "visible";
+
+            /*在app已安装的情况尝试拉起app*/
+        //    m.schemeWakeup();
+            /*用户点击某个按钮时(假定按钮id为downloadButton)，安装app*/
+        //    button.onclick = function() {
+        //       m.wakeupOrInstall();
+        //        return false;
+        //    }
+        // }
+    }, data);
+
+</script>
+
+
 hello world
-<html/>
+</html>
